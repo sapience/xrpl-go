53f62fcdf6b3b699ce0026c616a81ed83960becc
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-08-29 17:19:48 +0200

    feat(examples): add faucet examples

---
+++ examples/faucet/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+
+	addresscodec "github.com/Peersyst/xrpl-go/address-codec"
+	"github.com/Peersyst/xrpl-go/xrpl"
+	"github.com/Peersyst/xrpl-go/xrpl/client/websocket"
+	"github.com/Peersyst/xrpl-go/xrpl/faucet"
+)
+
+func main() {
+
+	fmt.Println("Funding wallet on testnet:\n")
+
+	testnetFaucet := faucet.NewTestnetFaucetProvider()
+
+	testnetClientCfg := websocket.NewWebsocketClientConfig().
+		WithHost("wss://s.altnet.rippletest.net:51233").
+		WithFaucetProvider(testnetFaucet)
+
+	testnetClient := websocket.NewWebsocketClient(testnetClientCfg)
+
+	wallet, err := xrpl.NewWallet(addresscodec.ED25519)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	balance, err := testnetClient.GetXrpBalance(wallet.ClassicAddress)
+	if err != nil {
+		balance = "0"
+	}
+
+	fmt.Println("Balance", wallet.ClassicAddress, balance)
+
+	
+
+	fmt.Println("Funding wallet", wallet.ClassicAddress)
+	err = testnetClient.FundWallet(&wallet)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	balance, err = testnetClient.GetXrpBalance(wallet.ClassicAddress)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Balance", wallet.ClassicAddress, balance)
+
+
+	fmt.Println("Funding wallet on devnet:\n")
+
+	devnetFaucet := faucet.NewDevnetFaucetProvider()
+
+	clientCfg := websocket.NewWebsocketClientConfig().
+		WithHost("wss://s.devnet.rippletest.net:51233").
+		WithFaucetProvider(devnetFaucet)
+
+	devnetClient := websocket.NewWebsocketClient(clientCfg)
+
+	balance, err = devnetClient.GetXrpBalance(wallet.ClassicAddress)
+	if err != nil {
+		balance = "0"
+	}
+
+	fmt.Println("Balance", wallet.ClassicAddress, balance)
+
+	
+	fmt.Println("Funding wallet", wallet.ClassicAddress)
+	err = devnetClient.FundWallet(&wallet)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	balance, err = devnetClient.GetXrpBalance(wallet.ClassicAddress)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Balance", wallet.ClassicAddress, balance)
+
+
+}
