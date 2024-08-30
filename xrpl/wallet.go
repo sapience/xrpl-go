4f65bacab4a46d4dd065c50b2b41fe571d8c234b
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-08-30 17:27:18 +0200

    fix(xrpl): disable wallet non-implemented methods

--- xrpl/wallet.go
+++ xrpl/wallet.go
@@ -2,16 +2,11 @@ package xrpl
 
 import (
 	"encoding/hex"
-	"errors"
-	"fmt"
-	"strings"
 
 	addresscodec "github.com/Peersyst/xrpl-go/address-codec"
 	binarycodec "github.com/Peersyst/xrpl-go/binary-codec"
 	"github.com/Peersyst/xrpl-go/keypairs"
 	"github.com/Peersyst/xrpl-go/xrpl/hash"
-	"github.com/tyler-smith/go-bip32"
-	"github.com/tyler-smith/go-bip39"
 )
 
 // A utility for deriving a wallet composed of a keypair (publicKey/privateKey).
@@ -68,57 +63,57 @@ func NewWalletFromSecret(seed string) (Wallet, error) {
 	return NewWalletFromSeed(seed, "")
 }
 
-// Derives a wallet from a bip39 or RFC1751 mnemonic (Defaults to bip39).
-// Returns a Wallet object. If an error occurs, it will be returned.
-func NewWalletFromMnemonic(mnemonic string) (*Wallet, error) {
-	// Validate the mnemonic
-	if !bip39.IsMnemonicValid(mnemonic) {
-		return nil, errors.New("invalid mnemonic")
-	}
+// // Derives a wallet from a bip39 or RFC1751 mnemonic (Defaults to bip39).
+// // Returns a Wallet object. If an error occurs, it will be returned.
+// func NewWalletFromMnemonic(mnemonic string) (*Wallet, error) {
+// 	// Validate the mnemonic
+// 	if !bip39.IsMnemonicValid(mnemonic) {
+// 		return nil, errors.New("invalid mnemonic")
+// 	}
 
-	// Generate seed from mnemonic
-	seed := bip39.NewSeed(mnemonic, "")
+// 	// Generate seed from mnemonic
+// 	seed := bip39.NewSeed(mnemonic, "")
 
-	// Derive the master key
-	masterKey, err := bip32.NewMasterKey(seed)
-	if err != nil {
-		return nil, err
-	}
+// 	// Derive the master key
+// 	masterKey, err := bip32.NewMasterKey(seed)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	// Derive the key using the path m/44'/144'/0'/0/0
-	path := []uint32{
-		44 + bip32.FirstHardenedChild,
-		144 + bip32.FirstHardenedChild,
-		bip32.FirstHardenedChild,
-		0,
-		0,
-	}
+// 	// Derive the key using the path m/44'/144'/0'/0/0
+// 	path := []uint32{
+// 		44 + bip32.FirstHardenedChild,
+// 		144 + bip32.FirstHardenedChild,
+// 		bip32.FirstHardenedChild,
+// 		0,
+// 		0,
+// 	}
 
-	key := masterKey
-	for _, childNum := range path {
-		key, err = key.NewChildKey(childNum)
-		if err != nil {
-			return nil, err
-		}
-	}
+// 	key := masterKey
+// 	for _, childNum := range path {
+// 		key, err = key.NewChildKey(childNum)
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 	}
 
-	// Convert the private key to the format expected by the XRPL library
-	privKey := strings.ToUpper(hex.EncodeToString(key.Key))
-	pubKey := strings.ToUpper(hex.EncodeToString(key.PublicKey().Key))
+// 	// Convert the private key to the format expected by the XRPL library
+// 	privKey := strings.ToUpper(hex.EncodeToString(key.Key))
+// 	pubKey := strings.ToUpper(hex.EncodeToString(key.PublicKey().Key))
 
-	// Derive classic address
-	classicAddr, err := keypairs.DeriveClassicAddress(pubKey)
-	if err != nil {
-		return nil, err
-	}
+// 	// Derive classic address
+// 	classicAddr, err := keypairs.DeriveClassicAddress(pubKey)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	return &Wallet{
-		PublicKey:      pubKey,
-		PrivateKey:     fmt.Sprintf("00%s", privKey),
-		ClassicAddress: classicAddr,
-		Seed:           "", // We don't have the seed in this case
-	}, nil
-}
+// 	return &Wallet{
+// 		PublicKey:      pubKey,
+// 		PrivateKey:     fmt.Sprintf("00%s", privKey),
+// 		ClassicAddress: classicAddr,
+// 		Seed:           "", // We don't have the seed in this case
+// 	}, nil
+// }
 
 // Signs a transaction offline.
 // In order for a transaction to be validated, it must be signed by the account sending the transaction to prove
@@ -162,11 +157,11 @@ func (w *Wallet) GetAddress() string {
 // Verifies a signed transaction offline.
 // Returns a boolean indicating if the transaction is valid and an error if it is not.
 // If the transaction is signed with a public key, the public key must match the one in the transaction.
-func (w *Wallet) VerifyTransaction(tx map[string]any) (bool, error) {
-	return false, errors.New("not implemented")
-}
+// func (w *Wallet) VerifyTransaction(tx map[string]any) (bool, error) {
+// 	return false, errors.New("not implemented")
+// }
 
-// Gets an X-address in Testnet/Mainnet format.
-func (w *Wallet) GetXAddress() (string, error) {
-	return "", errors.New("not implemented")
-}
+// // Gets an X-address in Testnet/Mainnet format.
+// func (w *Wallet) GetXAddress() (string, error) {
+// 	return "", errors.New("not implemented")
+// }
