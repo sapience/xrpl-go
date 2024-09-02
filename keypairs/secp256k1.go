829bd7e5f00ad9bf8af7ce381b60a5418c0d5441
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-09-02 16:46:39 +0200

    feat(keypairs): add secp256k1 deriveKeypair method

--- keypairs/secp256k1.go
+++ keypairs/secp256k1.go
@@ -2,13 +2,13 @@ package keypairs
 
 import (
 	"crypto/sha512"
+	"errors"
 	"math/big"
 
-	addresscodec "github.com/Peersyst/xrpl-go/address-codec"
 	"github.com/btcsuite/btcd/btcec/v2"
 )
 
-var _ CryptoImplementation = (*secp256k1Alg)(nil)
+// var _ CryptoImplementation = (*secp256k1Alg)(nil)
 
 type secp256k1Alg struct{}
 
@@ -49,32 +49,31 @@ func deriveScalar(bytes []byte, discrim *big.Int) *big.Int {
 	panic("impossible unicorn ;)")
 }
 
-func (c *secp256k1Alg) derivateKeypair(seed []byte, validator bool, accountIndex uint32) (string, string, error) {
+func (c *secp256k1Alg) deriveKeypair(seed []byte, validator bool) (string, string, error) {
 	curve := btcec.S256()
 	order := curve.N
 
 	privateGen := deriveScalar(seed, nil)
 
 	if validator {
-		return "", "", &secp256k1ValidatorError{}
+		return "", "", errors.New("validator keypair derivation not supported")
 	}
 
 	rootPrivateKey, _ := btcec.PrivKeyFromBytes(privateGen.Bytes())
 
-	derivatedScalar := deriveScalar(rootPrivateKey.PubKey().SerializeCompressed(), big.NewInt(int64(accountIndex)))
+	derivatedScalar := deriveScalar(rootPrivateKey.PubKey().SerializeCompressed(), big.NewInt(0))
 	scalarWithPrivateGen := derivatedScalar.Add(derivatedScalar, privateGen)
 	privateKey := scalarWithPrivateGen.Mod(scalarWithPrivateGen, order)
 
 	privKeyBytes := privateKey.Bytes()
-	privKeyBytes = append([]byte{addresscodec.SECP256K1}, privKeyBytes...)
 	private := formatKey(privKeyBytes)
 
 	_, pubKey := btcec.PrivKeyFromBytes(privKeyBytes)
 	
+	pubKeyBytes := pubKey.SerializeCompressed()
+	public := formatKey(pubKeyBytes)
 
-	
-
-
+	private = "00" + private
 	return private, public, nil
 }
 
---
+++ keypairs/secp256k1_test.go
@@ -0,0 +1,70 @@
+package keypairs
+
+import (
+	"errors"
+	"testing"
+
+	addresscodec "github.com/Peersyst/xrpl-go/address-codec"
+)
+
+func TestSecp256k1_deriveKeypair(t *testing.T) {
+	testCases := []struct {
+		name            string
+		seed            string
+		validator       bool
+		expectedPrivKey string
+		expectedPubKey  string
+		expectedErr     error
+	}{
+		{
+			name:            "valid seed (1)",
+			seed:            "sntbkd2DsouBx8BAdJdi35p1HRw6h",
+			validator:       false,
+			expectedPubKey:  "02950F4710101A25073BF37086D73FBBD00C7A6B0F91097D8F0BC6D268C400D56E",
+			expectedPrivKey: "00B167A9F3B9E60A4F93695713682C102438620AA1785C3AE635F53E5B6261071A",
+			expectedErr:     nil,
+		},
+		{
+			name:            "valid seed (2)",
+			seed:            "shSDdnXqsS7zAjbdWX86fT6H5oCxK",
+			validator:       false,
+			expectedPubKey:  "031FBCFDD2EC6C2EDFBBA3866BDBAC28E5253C6A01FE9EFF8CAAE01871F009E837",
+			expectedPrivKey: "00A3D1513DBE784107428B363A1F8EAF1377AB63D4D137AB9E28E0BC614C71D8C0",
+			expectedErr:     nil,
+		},
+		{
+			name:            "validator set to true",
+			seed:            "shSDdnXqsS7zAjbdWX86fT6H5oCxK",
+			validator:       true,
+			expectedPubKey:  "",
+			expectedPrivKey: "",
+			expectedErr:     errors.New("validator keypair derivation not supported"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			secp256k1 := secp256k1Alg{}
+			seedBytes, _, err := addresscodec.DecodeSeed(tc.seed)
+			if err != nil {
+				t.Fatal(err)
+			}
+			privKey, pubKey, err := secp256k1.deriveKeypair(seedBytes, tc.validator)
+			if tc.expectedErr != nil {
+				if err == nil || err.Error() != tc.expectedErr.Error() {
+					t.Fatalf("expected error %v, got %v", tc.expectedErr, err)
+				}
+			} else {
+				if err != nil {
+					t.Fatalf("expected valid keypair, got error: %v", err)
+				}
+				if privKey != tc.expectedPrivKey {
+					t.Errorf("expected private key %s, got %s", tc.expectedPrivKey, privKey)
+				}
+				if pubKey != tc.expectedPubKey {
+					t.Errorf("expected public key %s, got %s", tc.expectedPubKey, pubKey)
+				}
+			}
+		})
+	}
+}
