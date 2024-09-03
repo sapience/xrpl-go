088fb8c69da522b49e7244ca641b0ef11776e5b3
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-09-03 11:20:34 +0200

    fix(xrpl): flat memos

--- xrpl/model/transactions/memo.go
+++ xrpl/model/transactions/memo.go
@@ -4,8 +4,39 @@ type MemoWrapper struct {
 	Memo Memo
 }
 
+type FlatMemoWrapper map[string]interface{}
+
 type Memo struct {
 	MemoData   string `json:",omitempty"`
 	MemoFormat string `json:",omitempty"`
 	MemoType   string `json:",omitempty"`
+}
+
+type FlatMemo map[string]interface{}
+
+func (mw *MemoWrapper) Flatten() FlatMemoWrapper {
+	if mw.Memo != (Memo{}) {
+		flattened := make(FlatMemoWrapper)
+		flattened["Memo"] = mw.Memo.Flatten()
+		return flattened
+	}
+	return nil
+}
+
+func (m *Memo) Flatten() FlatMemo {
+	flattened := make(FlatMemo)
+
+	if m.MemoData != "" {
+		flattened["MemoData"] = m.MemoData
+	}
+
+	if m.MemoFormat != "" {
+		flattened["MemoFormat"] = m.MemoFormat
+	}
+
+	if m.MemoType != "" {
+		flattened["MemoType"] = m.MemoType
+	}
+
+	return flattened
 }
