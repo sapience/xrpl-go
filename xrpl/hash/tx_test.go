9c5bc013a66ba67356e7f4c22c171a276c9ee889
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-10-01 12:12:38 +0200

    refactor(hash): refactor hash module

--- xrpl/hash/tx_test.go
+++ xrpl/hash/tx_test.go
@@ -2,7 +2,7 @@ package hash
 
 import "testing"
 
-func TestHashSignedTx(t *testing.T) {
+func TestHashTxBlob(t *testing.T) {
 	testCases := []struct {
 		name    string
 		txBlob  string
@@ -25,7 +25,7 @@ func TestHashSignedTx(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			got, err := HashSignedTx(tc.txBlob)
+			got, err := HashTxBlob(tc.txBlob)
 			if (err != nil) != tc.wantErr {
 				t.Errorf("HashSignedTx() error = %v, wantErr %v", err, tc.wantErr)
 				return
