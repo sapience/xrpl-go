a82601476933b93683ba5fd9605b1fe5ecf8a2b9
Florent Uzio <fuzio@ripple.com>
2024-10-01 14:25:21 +0400

    condensed falsy conditions in objects.go

--- xrpl/utils/objects.go
+++ xrpl/utils/objects.go
@@ -2,13 +2,8 @@ package utils
 
 // OnlyHasFields checks if the given object has only the specified fields or a subset of them.
 func ObjectOnlyHasFields(obj map[string]interface{}, fields []string) bool {
-	// if obj is nil, return false
-	if obj == nil {
-		return false
-	}
-
-	// if obj is empty but not fields, return false
-	if len(obj) == 0 && len(fields) > 0 {
+	// if obj is nil or obj is empty but not fields, return false
+	if obj == nil || (len(obj) == 0 && len(fields) > 0) {
 		return false
 	}
 
