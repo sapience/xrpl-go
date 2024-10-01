01f5bd06ac63861af50bfe85abdc4f56e9a468ef
Florent Uzio <fuzio@ripple.com>
2024-10-01 14:22:38 +0400

    removed redundant map check

--- xrpl/model/transactions/validations_xrpl_objects.go
+++ xrpl/model/transactions/validations_xrpl_objects.go
@@ -75,13 +75,11 @@ func IsAmount(amount interface{}) bool {
 
 // IsIssuedCurrency checks if the given object is a valid IssuedCurrency object.
 func IsIssuedCurrency(input interface{}) bool {
-	// Check if the input is a map.
-	if !typecheck.IsMap(input) {
+	// Type the input as a map.
+	i, ok := input.(map[string]interface{})
+	if !ok {
 		return false
 	}
-
-	// Type the input as a map.
-	i := input.(map[string]interface{})
 
 	return len(maputils.GetKeys(i)) == ISSUED_CURRENCY_SIZE &&
 		typecheck.IsString(i["value"]) &&
