a13db61d2d77569c6e88964e9ea489f4b1a210fd
Florent Uzio <fuzio@ripple.com>
2024-10-03 14:17:04 +0400

    updated clawback tests

--- xrpl/model/transactions/clawback_test.go
+++ xrpl/model/transactions/clawback_test.go
@@ -90,89 +90,99 @@ func TestClawbackFlatten(t *testing.T) {
 	}
 }
 func TestClawbackValidate(t *testing.T) {
-	// Valid Clawback transaction
-	validClawback := Clawback{
-		BaseTx: BaseTx{
-			Account:         "abcdef",
-			TransactionType: ClawbackTx,
-			Fee:             types.XRPCurrencyAmount(1),
-			Sequence:        1234,
-			SigningPubKey:   "ghijk",
-			TxnSignature:    "A1B2C3D4E5F6",
+	tests := []struct {
+		name       string
+		clawback   Clawback
+		shouldPass bool
+	}{
+		{
+			name: "Valid Clawback transaction",
+			clawback: Clawback{
+				BaseTx: BaseTx{
+					Account:         "rnLYcEcYw2r3w6BDsFDSScoFmvZXbwa6EQ",
+					TransactionType: ClawbackTx,
+					Fee:             types.XRPCurrencyAmount(1),
+					Sequence:        1234,
+					SigningPubKey:   "ghijk",
+					TxnSignature:    "A1B2C3D4E5F6",
+				},
+				Amount: types.IssuedCurrencyAmount{
+					Issuer:   "rhsTg7mm7v3oEGrF85n1KdB3JjCk5KPT4M",
+					Currency: "USD",
+					Value:    "1",
+				},
+			},
+			shouldPass: true,
 		},
-		Amount: types.IssuedCurrencyAmount{
-			Issuer:   "def",
-			Currency: "USD",
-			Value:    "1",
+		{
+			name: "Clawback transaction with missing Amount field",
+			clawback: Clawback{
+				BaseTx: BaseTx{
+					Account:         "rnLYcEcYw2r3w6BDsFDSScoFmvZXbwa6EQ",
+					TransactionType: ClawbackTx,
+					Fee:             types.XRPCurrencyAmount(1),
+					Sequence:        1234,
+					SigningPubKey:   "ghijk",
+					TxnSignature:    "A1B2C3D4E5F6",
+				},
+			},
+			shouldPass: false,
 		},
-	}
-
-	valid, err := validClawback.Validate()
-	if err != nil {
-		t.Errorf("Validation failed for valid Clawback transaction: %s", err.Error())
-	}
-	if !valid {
-		t.Error("Validation should pass for valid Clawback transaction")
-	}
-
-	// Clawback transaction with missing Amount field
-	missingAmount := Clawback{
-		BaseTx: BaseTx{
-			Account:         "abcdef",
-			TransactionType: ClawbackTx,
-			Fee:             types.XRPCurrencyAmount(1),
-			Sequence:        1234,
-			SigningPubKey:   "ghijk",
-			TxnSignature:    "A1B2C3D4E5F6",
+		{
+			name: "Clawback transaction with invalid Amount",
+			clawback: Clawback{
+				BaseTx: BaseTx{
+					Account:         "rL2ek7KyeTk6NiyJcxYFrfiPcv8PFVoqgR",
+					TransactionType: ClawbackTx,
+					Fee:             types.XRPCurrencyAmount(1),
+					Sequence:        1234,
+					SigningPubKey:   "ghijk",
+					TxnSignature:    "A1B2C3D4E5F6",
+				},
+				Amount: types.IssuedCurrencyAmount{
+					Issuer:   "rnLYcEcYw2r3w6BDsFDSScoFmvZXbwa6EQ",
+					Currency: "USD",
+					Value:    "invalid",
+				},
+			},
+			shouldPass: false,
 		},
-	}
-
-	valid, err = missingAmount.Validate()
-	if err == nil || valid {
-		t.Error("Validation should fail for Clawback transaction with missing Amount field")
-	}
-
-	// Clawback transaction with invalid Amount
-	invalidAmount := Clawback{
-		BaseTx: BaseTx{
-			Account:         "abcdef",
-			TransactionType: ClawbackTx,
-			Fee:             types.XRPCurrencyAmount(1),
-			Sequence:        1234,
-			SigningPubKey:   "ghijk",
-			TxnSignature:    "A1B2C3D4E5F6",
-		},
-		Amount: types.IssuedCurrencyAmount{
-			Issuer:   "def",
-			Currency: "USD",
-			Value:    "invalid",
-		},
-	}
-
-	valid, err = invalidAmount.Validate()
-	if err == nil || valid {
-		t.Error("Validation should fail for Clawback transaction with invalid Amount")
-	}
-
-	// Clawback transaction with Account same as the issuer
-	invalidAccount := Clawback{
-		BaseTx: BaseTx{
-			Account:         "abcdef",
-			TransactionType: ClawbackTx,
-			Fee:             types.XRPCurrencyAmount(1),
-			Sequence:        1234,
-			SigningPubKey:   "ghijk",
-			TxnSignature:    "A1B2C3D4E5F6",
-		},
-		Amount: types.IssuedCurrencyAmount{
-			Issuer:   "abcdef",
-			Currency: "USD",
-			Value:    "1",
+		{
+			name: "Clawback transaction with Account same as the issuer",
+			clawback: Clawback{
+				BaseTx: BaseTx{
+					Account:         "rL2ek7KyeTk6NiyJcxYFrfiPcv8PFVoqgR",
+					TransactionType: ClawbackTx,
+					Fee:             types.XRPCurrencyAmount(1),
+					Sequence:        1234,
+					SigningPubKey:   "ghijk",
+					TxnSignature:    "A1B2C3D4E5F6",
+				},
+				Amount: types.IssuedCurrencyAmount{
+					Issuer:   "rL2ek7KyeTk6NiyJcxYFrfiPcv8PFVoqgR",
+					Currency: "USD",
+					Value:    "1",
+				},
+			},
+			shouldPass: false,
 		},
 	}
 
-	valid, err = invalidAccount.Validate()
-	if err == nil || valid {
-		t.Error("Validation should fail for Clawback transaction with Account same as the issuer")
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			valid, err := tt.clawback.Validate()
+			if tt.shouldPass {
+				if err != nil {
+					t.Errorf("Validation failed for valid Clawback transaction with error: %s", err.Error())
+				}
+				if !valid {
+					t.Error("Validation should pass for valid Clawback transaction")
+				}
+			} else {
+				if err == nil || valid {
+					t.Error("Validation should fail for invalid Clawback transaction")
+				}
+			}
+		})
 	}
 }
