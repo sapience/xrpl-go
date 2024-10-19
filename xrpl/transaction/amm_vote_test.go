e613a2ab9ff05525e97e39443446c46f5ab8d946
Florent Uzio <fuzio@ripple.com>
2024-10-19 22:45:35 +0400

    AMMVote Flatten and TxType

--- xrpl/transaction/amm_vote_test.go
+++ xrpl/transaction/amm_vote_test.go
@@ -1,44 +1,57 @@
 package transaction
 
 import (
-	"encoding/json"
-	"reflect"
 	"testing"
 
+	ledger "github.com/Peersyst/xrpl-go/xrpl/ledger-entry-types"
 	"github.com/Peersyst/xrpl-go/xrpl/testutil"
 	"github.com/Peersyst/xrpl-go/xrpl/transaction/types"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestAMMVoteTransaction(t *testing.T) {
-	s := AMMVote{
+func TestAMMVote_TxType(t *testing.T) {
+	tx := &AMMVote{}
+	assert.Equal(t, AMMVoteTx, tx.TxType())
+}
+func TestAMMVote_Flatten(t *testing.T) {
+	tx := &AMMVote{
 		BaseTx: BaseTx{
-			Account:         "abcdef",
-			TransactionType: AMMVoteTx,
-			Fee:             types.XRPCurrencyAmount(1),
-			Sequence:        1234,
-			SigningPubKey:   "ghijk",
-			TxnSignature:    "A1B2C3D4E5F6",
+			Account:         "rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm",
+			TransactionType: "AMMVote",
+			Fee:             types.XRPCurrencyAmount(10),
+			Flags:           2147483648,
+			Sequence:        8,
 		},
+		Asset: ledger.Asset{
+			Currency: "XRP",
+		},
+		Asset2: ledger.Asset{
+			Currency: "TST",
+			Issuer:   "rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd",
+		},
+		TradingFee: 600,
 	}
 
-	j := `{
-	"Account": "abcdef",
-	"TransactionType": "AMMVote",
-	"Fee": "1",
-	"Sequence": 1234,
-	"SigningPubKey": "ghijk",
-	"TxnSignature": "A1B2C3D4E5F6"
-}`
+	flattened := tx.Flatten()
 
-	if err := testutil.SerializeAndDeserialize(t, s, j); err != nil {
-		t.Error(err)
-	}
+	expected := `{
+		"Account":         "rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm",
+		"Fee":             "10",
+		"Flags":           2147483648,
+		"Sequence":        8,
+		"TransactionType": "AMMVote",
+		"Asset": {
+			"currency": "XRP"
+		},
+		"Asset2": {
+			"currency": "TST",
+			"issuer":   "rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd"
+		},
+		"TradingFee": 600
+	}`
 
-	tx, err := UnmarshalTx(json.RawMessage(j))
+	err := testutil.CompareFlattenAndExpected(flattened, []byte(expected))
 	if err != nil {
-		t.Errorf("UnmarshalTx error: %s", err.Error())
-	}
-	if !reflect.DeepEqual(tx, &s) {
-		t.Error("UnmarshalTx result differs from expected")
+		t.Error(err)
 	}
 }
