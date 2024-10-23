d7573e793f3fc522fa84d5786a493bb0dcbb44d9
Florent Uzio <fuzio@ripple.com>
2024-10-23 15:53:00 +0400

    TicketCreate Validate and 100% tests

--- xrpl/transaction/ticket_create.go
+++ xrpl/transaction/ticket_create.go
@@ -1,19 +1,36 @@
 package transaction
 
+import "errors"
+
 // A TicketCreate transaction sets aside one or more sequence numbers as Tickets.
+//
+// Example:
+//
+//	```json
+//
+//	{
+//	    "TransactionType": "TicketCreate",
+//	    "Account": "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+//	    "Fee": "10",
+//	    "Sequence": 381,
+//	    "TicketCount": 10
+//	}
+//
+// ```
 type TicketCreate struct {
 	// Base transaction fields
 	BaseTx
-
 	//How many Tickets to create. This must be a positive number and cannot cause
 	// the account to own more than 250 Tickets after executing this transaction.
 	TicketCount uint32
 }
 
+// TxType returns the type of the transaction (TicketCreate).
 func (*TicketCreate) TxType() TxType {
 	return TicketCreateTx
 }
 
+// Flatten returns the flattened map of the AMMVote transaction.
 func (t *TicketCreate) Flatten() FlatTransaction {
 	flattened := t.BaseTx.Flatten()
 
@@ -24,4 +41,18 @@ func (t *TicketCreate) Flatten() FlatTransaction {
 	}
 
 	return flattened
+}
+
+// Validates the TicketCreate transaction and makes sure all the fields are correct.
+func (t *TicketCreate) Validate() (bool, error) {
+	_, err := t.BaseTx.Validate()
+	if err != nil {
+		return false, err
+	}
+
+	if t.TicketCount == 0 || t.TicketCount > 250 {
+		return false, errors.New("ticketCount must be greater than 0 and less than or equal to 250")
+	}
+
+	return true, nil
 }
--- xrpl/transaction/ticket_create_test.go
+++ xrpl/transaction/ticket_create_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/Peersyst/xrpl-go/xrpl/testutil"
 	"github.com/Peersyst/xrpl-go/xrpl/transaction/types"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTicketCreateTx(t *testing.T) {
@@ -42,7 +43,7 @@ func TestTicketCreateTx(t *testing.T) {
 
 }
 
-func TestTicketCreateFlatten(t *testing.T) {
+func TestTicketCreate_Flatten(t *testing.T) {
 	s := TicketCreate{
 		BaseTx: BaseTx{
 			Account:         "ra5nK24KXen9AHvsdFTKHSANinZseWnPcX",
@@ -65,5 +66,79 @@ func TestTicketCreateFlatten(t *testing.T) {
 
 	if !reflect.DeepEqual(flattened, expected) {
 		t.Errorf("Flatten result differs from expected: %v, %v", flattened, expected)
+	}
+}
+
+func TestTicketCreate_TxType(t *testing.T) {
+	tx := &TicketCreate{}
+	assert.Equal(t, TicketCreateTx, tx.TxType())
+}
+func TestTicketCreate_Validate(t *testing.T) {
+	tests := []struct {
+		name      string
+		ticket    TicketCreate
+		wantValid bool
+	}{
+		{
+			name: "valid ticket count",
+			ticket: TicketCreate{
+				BaseTx: BaseTx{
+					Account:         "abc",
+					TransactionType: TicketCreateTx,
+					Fee:             types.XRPCurrencyAmount(10),
+					Sequence:        50,
+				},
+				TicketCount: 5,
+			},
+			wantValid: true,
+		},
+		{
+			name: "Invalid BaseTx",
+			ticket: TicketCreate{
+				BaseTx: BaseTx{
+					Account:         "",
+					TransactionType: TicketCreateTx,
+					Fee:             types.XRPCurrencyAmount(10),
+					Sequence:        50,
+				},
+				TicketCount: 5,
+			},
+			wantValid: false,
+		},
+		{
+			name: "ticket count zero",
+			ticket: TicketCreate{
+				BaseTx: BaseTx{
+					Account:         "abc",
+					TransactionType: TicketCreateTx,
+					Fee:             types.XRPCurrencyAmount(10),
+					Sequence:        50,
+				},
+				TicketCount: 0,
+			},
+			wantValid: false,
+		},
+		{
+			name: "ticket count exceeds limit",
+			ticket: TicketCreate{
+				BaseTx: BaseTx{
+					Account:         "abc",
+					TransactionType: TicketCreateTx,
+					Fee:             types.XRPCurrencyAmount(10),
+					Sequence:        50,
+				},
+				TicketCount: 251,
+			},
+			wantValid: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			valid, err := tt.ticket.Validate()
+			if valid != tt.wantValid {
+				t.Errorf("Validate() valid = %v, want %v, err: %v", valid, tt.wantValid, err)
+			}
+		})
 	}
 }
