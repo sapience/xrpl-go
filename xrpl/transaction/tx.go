19a523dfc9852844a530a43673dfe79167886905
Florent Uzio <fuzio@ripple.com>
2024-10-25 13:50:41 +0400

    removed UnmarshalTx and subsequent uses in tests files

--- xrpl/transaction/tx.go
+++ xrpl/transaction/tx.go
@@ -1,7 +1,6 @@
 package transaction
 
 import (
-	"encoding/json"
 	"fmt"
 
 	addresscodec "github.com/Peersyst/xrpl-go/address-codec"
@@ -163,107 +162,6 @@ func (tx *BaseTx) Flatten() FlatTransaction {
 	}
 
 	return flattened
-}
-
-func UnmarshalTx(data json.RawMessage) (Tx, error) {
-	if len(data) == 0 {
-		return nil, nil
-	}
-	if data[0] == '"' {
-		var ret TxHash
-		if err := json.Unmarshal(data, &ret); err != nil {
-			return nil, err
-		}
-		return &ret, nil
-	} else if data[0] != '{' {
-		// TODO error verbosity/record failed json
-		return nil, fmt.Errorf("unexpected tx format; must be tx object or hash string")
-	}
-	// TODO AMM endpoint support
-	type txTypeParser struct {
-		TransactionType TxType
-		TxBlob          string `json:"tx_blob"`
-	}
-	var txType txTypeParser
-	if err := json.Unmarshal(data, &txType); err != nil {
-		return nil, err
-	}
-	if len(txType.TxBlob) > 0 && len(txType.TransactionType) == 0 {
-		return &Binary{
-			TxBlob: txType.TxBlob,
-		}, nil
-	}
-
-	fmt.Println("TxType", txType.TransactionType)
-	var tx Tx
-	switch txType.TransactionType {
-	case AMMBidTx:
-		tx = &AMMBid{}
-	case AMMCreateTx:
-		tx = &AMMCreate{}
-	case AMMDepositTx:
-		tx = &AMMDeposit{}
-	case AMMVoteTx:
-		tx = &AMMVote{}
-	case AMMWithdrawTx:
-		tx = &AMMWithdraw{}
-	case AccountSetTx:
-		tx = &AccountSet{}
-	case AccountDeleteTx:
-		tx = &AccountDelete{}
-	case CheckCancelTx:
-		tx = &CheckCancel{}
-	case CheckCashTx:
-		tx = &CheckCash{}
-	case CheckCreateTx:
-		tx = &CheckCreate{}
-	case ClawbackTx:
-		tx = &Clawback{}
-	case DepositPreauthTx:
-		tx = &DepositPreauth{}
-	case EscrowCancelTx:
-		tx = &EscrowCancel{}
-	case EscrowCreateTx:
-		tx = &EscrowCreate{}
-	case EscrowFinishTx:
-		tx = &EscrowFinish{}
-	case NFTokenAcceptOfferTx:
-		tx = &NFTokenAcceptOffer{}
-	case NFTokenBurnTx:
-		tx = &NFTokenBurn{}
-	case NFTokenCancelOfferTx:
-		tx = &NFTokenCancelOffer{}
-	case NFTokenCreateOfferTx:
-		tx = &NFTokenCreateOffer{}
-	case NFTokenMintTx:
-		tx = &NFTokenMint{}
-	case OfferCreateTx:
-		tx = &OfferCreate{}
-	case OfferCancelTx:
-		tx = &OfferCancel{}
-	case PaymentTx:
-		tx = &Payment{}
-	case PaymentChannelClaimTx:
-		tx = &PaymentChannelClaim{}
-	case PaymentChannelCreateTx:
-		tx = &PaymentChannelCreate{}
-	case PaymentChannelFundTx:
-		tx = &PaymentChannelFund{}
-	case SetRegularKeyTx:
-		tx = &SetRegularKey{}
-	case SignerListSetTx:
-		tx = &SignerListSet{}
-	case TrustSetTx:
-		tx = &TrustSet{}
-	case TicketCreateTx:
-		tx = &TicketCreate{}
-	default:
-		return nil, fmt.Errorf("unsupported transaction type %s", txType.TransactionType)
-	}
-	if err := json.Unmarshal(data, tx); err != nil {
-		return nil, err
-	}
-	return tx, nil
 }
 
 func (tx *BaseTx) Validate() (bool, error) {
