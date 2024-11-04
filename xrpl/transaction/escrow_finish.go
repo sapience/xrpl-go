0ac77fd6ca1e48be87faa03ca57988331b917780
Florent Uzio <fuzio@ripple.com>
2024-11-04 10:19:21 +0400

    EscrowFinish added Flatten func

--- xrpl/transaction/escrow_finish.go
+++ xrpl/transaction/escrow_finish.go
@@ -4,19 +4,60 @@ import (
 	"github.com/Peersyst/xrpl-go/xrpl/transaction/types"
 )
 
+// Deliver XRP from a held payment to the recipient.
+//
+// Example:
+//
+// ```json
+//
+//	{
+//	    "Account": "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+//	    "TransactionType": "EscrowFinish",
+//	    "Owner": "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+//	    "OfferSequence": 7,
+//	    "Condition": "A0258020E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855810100",
+//	    "Fulfillment": "A0028000"
+//	}
+//
+// ````
 type EscrowFinish struct {
 	BaseTx
-	Owner         types.Address
+	// Address of the source account that funded the held payment.
+	Owner types.Address
+	// Transaction sequence of EscrowCreate transaction that created the held payment to finish.
 	OfferSequence uint
-	Condition     string `json:",omitempty"`
-	Fulfillment   string `json:",omitempty"`
+	// (Optional) Hex value matching the previously-supplied PREIMAGE-SHA-256 crypto-condition of the held payment.
+	Condition string `json:",omitempty"`
+	// Optional) Hex value of the PREIMAGE-SHA-256 crypto-condition fulfillment matching the held payment's Condition.
+	Fulfillment string `json:",omitempty"`
 }
 
+// TxType returns the transaction type for this transaction (EscrowFinish).
 func (*EscrowFinish) TxType() TxType {
 	return EscrowFinishTx
 }
 
-// TODO: Implement flatten
+// Flatten returns the flattened map of the EscrowFinish transaction.
 func (s *EscrowFinish) Flatten() FlatTransaction {
-	return nil
+	flattened := s.BaseTx.Flatten()
+
+	flattened["TransactionType"] = "EscrowFinish"
+
+	if s.Owner != "" {
+		flattened["Owner"] = s.Owner
+	}
+
+	if s.OfferSequence != 0 {
+		flattened["OfferSequence"] = s.OfferSequence
+	}
+
+	if s.Condition != "" {
+		flattened["Condition"] = s.Condition
+	}
+
+	if s.Fulfillment != "" {
+		flattened["Fulfillment"] = s.Fulfillment
+	}
+
+	return flattened
 }
--- xrpl/transaction/escrow_finish_test.go
+++ xrpl/transaction/escrow_finish_test.go
@@ -1,1 +1,12 @@
 package transaction
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEscrowFinish_TxType(t *testing.T) {
+	entry := &EscrowFinish{}
+	assert.Equal(t, EscrowFinishTx, entry.TxType())
+}
