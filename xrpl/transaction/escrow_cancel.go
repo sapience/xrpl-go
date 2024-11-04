f6ae538b82e6300923a9c8620a7259e3437a49b5
Florent Uzio <fuzio@ripple.com>
2024-11-04 10:11:24 +0400

    EscrowCancel Flatten and tests

--- xrpl/transaction/escrow_cancel.go
+++ xrpl/transaction/escrow_cancel.go
@@ -4,17 +4,46 @@ import (
 	"github.com/Peersyst/xrpl-go/xrpl/transaction/types"
 )
 
+// Return escrowed XRP to the sender.
+//
+// Example:
+//
+// ```json
+//
+//	{
+//	    "Account": "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+//	    "TransactionType": "EscrowCancel",
+//	    "Owner": "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+//	    "OfferSequence": 7,
+//	}
+//
+// ```
 type EscrowCancel struct {
 	BaseTx
-	Owner         types.Address
+	// Address of the source account that funded the escrow payment.
+	Owner types.Address
+	// Transaction sequence (or Ticket number) of EscrowCreate transaction that created the escrow to cancel.
 	OfferSequence uint
 }
 
+// TxType returns the transaction type for this transaction (EscrowCancel).
 func (*EscrowCancel) TxType() TxType {
 	return EscrowCancelTx
 }
 
-// TODO: Implement flatten
+// Flatten returns the flattened map of the EscrowCancel transaction.
 func (s *EscrowCancel) Flatten() FlatTransaction {
-	return nil
+	flattened := s.BaseTx.Flatten()
+
+	flattened["TransactionType"] = "EscrowCancel"
+
+	if s.Owner != "" {
+		flattened["Owner"] = s.Owner
+	}
+
+	if s.OfferSequence != 0 {
+		flattened["OfferSequence"] = s.OfferSequence
+	}
+
+	return flattened
 }
--- xrpl/transaction/escrow_cancel_test.go
+++ xrpl/transaction/escrow_cancel_test.go
@@ -1,1 +1,87 @@
 package transaction
+
+import (
+	"testing"
+
+	"github.com/Peersyst/xrpl-go/xrpl/testutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEscrowCancel_TxType(t *testing.T) {
+	entry := &EscrowCancel{}
+	assert.Equal(t, EscrowCancelTx, entry.TxType())
+}
+
+func TestEscrowCancel_Flatten(t *testing.T) {
+	tests := []struct {
+		name     string
+		escrow   *EscrowCancel
+		expected string
+	}{
+		{
+			name: "complete EscrowCancel",
+			escrow: &EscrowCancel{
+				BaseTx: BaseTx{
+					Account: "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				},
+				Owner:         "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				OfferSequence: 7,
+			},
+			expected: `{
+				"TransactionType": "EscrowCancel",
+				"Account":         "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				"Owner":           "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				"OfferSequence":   7
+			}`,
+		},
+		{
+			name: "EscrowCancel without Owner",
+			escrow: &EscrowCancel{
+				BaseTx: BaseTx{
+					Account: "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				},
+				OfferSequence: 7,
+			},
+			expected: `{
+				"TransactionType": "EscrowCancel",
+				"Account": "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				"OfferSequence": 7
+			}`,
+		},
+		{
+			name: "EscrowCancel without OfferSequence",
+			escrow: &EscrowCancel{
+				BaseTx: BaseTx{
+					Account: "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				},
+				Owner: "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+			},
+			expected: `{
+				"TransactionType": "EscrowCancel",
+				"Account": "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				"Owner": "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn"
+			}`,
+		},
+		{
+			name: "EscrowCancel with only BaseTx",
+			escrow: &EscrowCancel{
+				BaseTx: BaseTx{
+					Account: "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				},
+			},
+			expected: `{
+				"TransactionType": "EscrowCancel",
+				"Account": "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn"
+			}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := testutil.CompareFlattenAndExpected(tt.escrow.Flatten(), []byte(tt.expected))
+			if err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
