652e27cfe25b96edde3eb563e07c16706095e1f7
Florent Uzio <fuzio@ripple.com>
2024-11-04 10:31:06 +0400

    EscrowFinish 100% coverage

--- xrpl/transaction/escrow_finish_test.go
+++ xrpl/transaction/escrow_finish_test.go
@@ -100,6 +100,17 @@ func TestEscrowFinish_Validate(t *testing.T) {
 			expected: true,
 		},
 		{
+			name: "Invalid EscrowFinish BaseTx",
+			entry: &EscrowFinish{
+				BaseTx: BaseTx{
+					Account: "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				},
+				Owner:         "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				OfferSequence: 7,
+			},
+			expected: false,
+		},
+		{
 			name: "Invalid Owner Address",
 			entry: &EscrowFinish{
 				BaseTx: BaseTx{
