8ef8e1d289209ca01961625327b168295604e476
Florent Uzio <fuzio@ripple.com>
2024-11-04 16:34:33 +0400

    SetRegularKey Flatten and Tests

--- xrpl/transaction/set_regular_key.go
+++ xrpl/transaction/set_regular_key.go
@@ -4,16 +4,45 @@ import (
 	"github.com/Peersyst/xrpl-go/xrpl/transaction/types"
 )
 
+// A SetRegularKey transaction assigns, changes, or removes the regular key pair associated with an account.
+//
+// You can protect your account by assigning a regular key pair to it and using it instead of the master key pair to sign transactions whenever possible.
+// If your regular key pair is compromised, but your master key pair is not, you can use a SetRegularKey transaction to regain control of your account.
+//
+// Example:
+//
+// ```json
+//
+//	{
+//	    "Flags": 0,
+//	    "TransactionType": "SetRegularKey",
+//	    "Account": "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+//	    "Fee": "12",
+//	    "RegularKey": "rAR8rR8sUkBoCZFawhkWzY4Y5YoyuznwD"
+//	}
+//
+// ```
 type SetRegularKey struct {
 	BaseTx
+	// (Optional) A base-58-encoded Address that indicates the regular key pair to be assigned to the account.
+	// If omitted, removes any existing regular key pair from the account. Must not match the master key pair for the address.
 	RegularKey types.Address `json:",omitempty"`
 }
 
+// TxType returns the transaction type for this transaction (SetRegularKey).
 func (*SetRegularKey) TxType() TxType {
 	return SetRegularKeyTx
 }
 
-// TODO: Implement flatten
+// Flatten returns the flattened map of the SetRegularKey transaction.
 func (s *SetRegularKey) Flatten() FlatTransaction {
-	return nil
+	flattened := s.BaseTx.Flatten()
+
+	flattened["TransactionType"] = "SetRegularKey"
+
+	if s.RegularKey != "" {
+		flattened["RegularKey"] = s.RegularKey
+	}
+
+	return flattened
 }
--- xrpl/transaction/set_regular_key_test.go
+++ xrpl/transaction/set_regular_key_test.go
@@ -1,1 +1,59 @@
 package transaction
+
+import (
+	"testing"
+
+	"github.com/Peersyst/xrpl-go/xrpl/testutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetRegularKey_TxType(t *testing.T) {
+	entry := &SetRegularKey{}
+	assert.Equal(t, SetRegularKeyTx, entry.TxType())
+}
+
+func TestSetRegularKey_Flatten(t *testing.T) {
+	tests := []struct {
+		name       string
+		regularKey *SetRegularKey
+		want       string
+	}{
+		{
+			name: "Valid SetRegularKey",
+			regularKey: &SetRegularKey{
+				BaseTx: BaseTx{
+					Account:         "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+					TransactionType: SetRegularKeyTx,
+				},
+				RegularKey: "rUpy3eEg8rqjqfUoLeBnZkscbKbFsKXC3v",
+			},
+			want: `{
+				"Account": "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				"TransactionType": "SetRegularKey",
+				"RegularKey":      "rUpy3eEg8rqjqfUoLeBnZkscbKbFsKXC3v"
+			}`,
+		},
+		{
+			name: "Without RegularKey",
+			regularKey: &SetRegularKey{
+				BaseTx: BaseTx{
+					Account:         "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+					TransactionType: SetRegularKeyTx,
+				},
+			},
+			want: `{
+				"Account": "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				"TransactionType": "SetRegularKey"
+			}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := testutil.CompareFlattenAndExpected(tt.regularKey.Flatten(), []byte(tt.want))
+			if err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
