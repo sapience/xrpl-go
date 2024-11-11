768e0856cde3326b40218c832b59cd9d612377e1
Florent Uzio <fuzio@ripple.com>
2024-11-11 13:38:08 +0400

    Flatten and tests for Cancel Offer

--- xrpl/transaction/nftoken_cancel_offer.go
+++ xrpl/transaction/nftoken_cancel_offer.go
@@ -4,16 +4,42 @@ import (
 	"github.com/Peersyst/xrpl-go/xrpl/transaction/types"
 )
 
+// The NFTokenCancelOffer transaction can be used to cancel existing token offers created using NFTokenCreateOffer.
+//
+// Example:
+//
+// ```json
+//
+//	{
+//		"TransactionType": "NFTokenCancelOffer",
+//		"Account": "ra5nK24KXen9AHvsdFTKHSANinZseWnPcX",
+//		"NFTokenOffers": [
+//			"9C92E061381C1EF37A8CDE0E8FC35188BFC30B1883825042A64309AC09F4C36D"
+//		]
+//	}
+//
+// ```
 type NFTokenCancelOffer struct {
 	BaseTx
-	NFTokenOffers []types.Hash256
+	// An array of IDs of the NFTokenOffer objects to cancel (not the IDs of NFToken objects, but the IDs of the NFTokenOffer objects).
+	// Each entry must be a different object ID of an NFTokenOffer object; the transaction is invalid if the array contains duplicate entries.
+	NFTokenOffers []types.NFTokenID
 }
 
+// TxType returns the type of the transaction (NFTokenCancelOffer).
 func (*NFTokenCancelOffer) TxType() TxType {
 	return NFTokenCancelOfferTx
 }
 
-// TODO: Implement flatten
+// Flatten returns a map of the NFTokenCancelOffer transaction fields.
 func (s *NFTokenCancelOffer) Flatten() FlatTransaction {
-	return nil
+	flattened := s.BaseTx.Flatten()
+
+	flattened["TransactionType"] = "NFTokenCancelOffer"
+
+	if len(s.NFTokenOffers) > 0 {
+		flattened["NFTokenOffers"] = s.NFTokenOffers
+	}
+
+	return flattened
 }
--- xrpl/transaction/nftoken_cancel_offer_test.go
+++ xrpl/transaction/nftoken_cancel_offer_test.go
@@ -1,1 +1,64 @@
 package transaction
+
+import (
+	"testing"
+
+	"github.com/Peersyst/xrpl-go/xrpl/testutil"
+	"github.com/Peersyst/xrpl-go/xrpl/transaction/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNFTokenCancelOffer_TxType(t *testing.T) {
+	tx := &NFTokenCancelOffer{}
+	assert.Equal(t, NFTokenCancelOfferTx, tx.TxType())
+}
+
+func TestNFTokenCancelOffer_Flatten(t *testing.T) {
+	tests := []struct {
+		name     string
+		tx       *NFTokenCancelOffer
+		expected string
+	}{
+		{
+			name: "Empty NFTokenOffers",
+			tx: &NFTokenCancelOffer{
+				BaseTx: BaseTx{
+					Account:         "ra5nK24KXen9AHvsdFTKHSANinZseWnPcX",
+					TransactionType: NFTokenCancelOfferTx,
+				},
+				NFTokenOffers: []types.NFTokenID{},
+			},
+			expected: `{
+				"Account": "ra5nK24KXen9AHvsdFTKHSANinZseWnPcX",
+				"TransactionType": "NFTokenCancelOffer"
+			}`,
+		},
+		{
+			name: "With NFTokenOffers",
+			tx: &NFTokenCancelOffer{
+				BaseTx: BaseTx{
+					Account: "ra5nK24KXen9AHvsdFTKHSANinZseWnPcX",
+				},
+				NFTokenOffers: []types.NFTokenID{
+					"9C92E061381C1EF37A8CDE0E8FC35188BFC30B1883825042A64309AC09F4C36D",
+				},
+			},
+			expected: `{
+				"Account": "ra5nK24KXen9AHvsdFTKHSANinZseWnPcX",
+				"TransactionType": "NFTokenCancelOffer",
+				"NFTokenOffers": [
+					"9C92E061381C1EF37A8CDE0E8FC35188BFC30B1883825042A64309AC09F4C36D"
+				]
+			}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := testutil.CompareFlattenAndExpected(tt.tx.Flatten(), []byte(tt.expected))
+			if err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
