2a738c55f764d93a459d98fedd01ac9a3a37b985
Florent Uzio <fuzio@ripple.com>
2024-11-11 14:02:45 +0400

    Burn NFT Flatten and tests

--- xrpl/transaction/nftoken_burn.go
+++ xrpl/transaction/nftoken_burn.go
@@ -4,17 +4,53 @@ import (
 	"github.com/Peersyst/xrpl-go/xrpl/transaction/types"
 )
 
+// The NFTokenBurn transaction is used to remove a NFToken object from the NFTokenPage in which it is being held, effectively removing the token from the ledger (burning it).
+//
+// The sender of this transaction must be the owner of the NFToken to burn; or, if the NFToken has the lsfBurnable flag enabled, can be the issuer or the issuer's authorized NFTokenMinter account instead.
+//
+// If this operation succeeds, the corresponding NFToken is removed.
+// If this operation empties the NFTokenPage holding the NFToken or results in consolidation, thus removing a NFTokenPage, the owner’s reserve requirement is reduced by one.
+//
+// Example:
+//
+// ```json
+//
+//	{
+//		"TransactionType": "NFTokenBurn",
+//		"Account": "rNCFjv8Ek5oDrNiMJ3pw6eLLFtMjZLJnf2",
+//		"Owner": "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B",
+//		"Fee": "10",
+//		"NFTokenID": "000B013A95F14B0044F78A264E41713C64B5F89242540EE208C3098E00000D65"
+//	  }
+//
+// ```
 type NFTokenBurn struct {
 	BaseTx
+	// The NFToken to be removed by this transaction.
 	NFTokenID types.NFTokenID
-	Owner     types.Address `json:",omitempty"`
+	// (Optional) The owner of the NFToken to burn. Only used if that owner is different than the account sending this transaction.
+	// The issuer or authorized minter can use this field to burn NFTs that have the lsfBurnable flag enabled.
+	Owner types.Address `json:",omitempty"`
 }
 
+// TxType returns the type of the transaction (NFTokenBurn).
 func (*NFTokenBurn) TxType() TxType {
 	return NFTokenBurnTx
 }
 
-// TODO: Implement flatten
-func (s *NFTokenBurn) Flatten() FlatTransaction {
-	return nil
+// Flatten returns a map of the NFTokenBurn transaction fields.
+func (n *NFTokenBurn) Flatten() FlatTransaction {
+	flattened := n.BaseTx.Flatten()
+
+	flattened["TransactionType"] = "NFTokenBurn"
+
+	if n.Owner != "" {
+		flattened["Owner"] = n.Owner
+	}
+
+	if n.NFTokenID != "" {
+		flattened["NFTokenID"] = n.NFTokenID
+	}
+
+	return flattened
 }
--- xrpl/transaction/nftoken_burn_test.go
+++ xrpl/transaction/nftoken_burn_test.go
@@ -1,1 +1,76 @@
 package transaction
+
+import (
+	"testing"
+
+	"github.com/Peersyst/xrpl-go/xrpl/testutil"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNFTokenBurn_TxType(t *testing.T) {
+	tx := &NFTokenBurn{}
+	assert.Equal(t, NFTokenBurnTx, tx.TxType())
+}
+
+func TestNFTokenBurn_Flatten(t *testing.T) {
+	tests := []struct {
+		name     string
+		nftBurn  *NFTokenBurn
+		expected string
+	}{
+		{
+			name: "Without Owner",
+			nftBurn: &NFTokenBurn{
+				BaseTx: BaseTx{
+					Account:         "rNCFjv8Ek5oDrNiMJ3pw6eLLFtMjZLJnf2",
+					TransactionType: NFTokenBurnTx,
+				},
+				NFTokenID: "000B013A95F14B0044F78A264E41713C64B5F89242540EE208C3098E00000D65",
+			},
+			expected: `{
+				"Account": "rNCFjv8Ek5oDrNiMJ3pw6eLLFtMjZLJnf2",
+				"TransactionType": "NFTokenBurn",
+				"NFTokenID": "000B013A95F14B0044F78A264E41713C64B5F89242540EE208C3098E00000D65"
+			}`,
+		},
+		{
+			name: "With Owner",
+			nftBurn: &NFTokenBurn{
+				BaseTx: BaseTx{
+					Account:         "rNCFjv8Ek5oDrNiMJ3pw6eLLFtMjZLJnf2",
+					TransactionType: NFTokenBurnTx,
+				},
+				NFTokenID: "000B013A95F14B0044F78A264E41713C64B5F89242540EE208C3098E00000D65",
+				Owner:     "rNCFjv8Ek5oDrNiMJ3pw6eLLFtMjZLJnf2",
+			},
+			expected: `{
+				"Account": "rNCFjv8Ek5oDrNiMJ3pw6eLLFtMjZLJnf2",
+				"TransactionType": "NFTokenBurn",
+				"NFTokenID": "000B013A95F14B0044F78A264E41713C64B5F89242540EE208C3098E00000D65",
+				"Owner": "rNCFjv8Ek5oDrNiMJ3pw6eLLFtMjZLJnf2"
+			}`,
+		},
+		{
+			name: "Without NFTokenID",
+			nftBurn: &NFTokenBurn{
+				BaseTx: BaseTx{
+					Account:         "rNCFjv8Ek5oDrNiMJ3pw6eLLFtMjZLJnf2",
+					TransactionType: NFTokenBurnTx,
+				},
+			},
+			expected: `{
+				"Account": "rNCFjv8Ek5oDrNiMJ3pw6eLLFtMjZLJnf2",
+				"TransactionType": "NFTokenBurn"
+			}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := testutil.CompareFlattenAndExpected(tt.nftBurn.Flatten(), []byte(tt.expected))
+			if err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
