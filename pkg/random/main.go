8c3250b7173857d347f6924718c848479c973c2d
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-11-11 15:26:59 +0100

    fix(pkg/random): add tests for `Randomizer` and reference comments

--- pkg/random/main.go
+++ pkg/random/main.go
@@ -5,19 +5,23 @@ import (
 	"io"
 )
 
+// Randomizer implements the io.Reader interface. A Randomizer can be used to generate random bytes.
 type Randomizer struct {
 	io.Reader
 }
 
-func NewRandomizer() *Randomizer {
-	return &Randomizer{
+// NewRandomizer returns a new Randomizer instance. It uses the crypto/rand package to generate random bytes.
+func NewRandomizer() Randomizer {
+	return Randomizer{
 		Reader: rand.Reader,
 	}
 }
 
-func (r *Randomizer) GenerateBytes(n int) ([]byte, error) {
+// GenerateBytes generates a n bytes slice of random bytes. It can return an error if the random bytes cannot be read.
+// For further information, see the io.Reader interface.
+func (r Randomizer) GenerateBytes(n int) ([]byte, error) {
 	b := make([]byte, n)
-	_, err := r.Read(b) //nolint
+	_, err := r.Read(b)
 	if err != nil {
 		return nil, err
 	}
