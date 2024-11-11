4fa4be421eb0676d87855e8864f33e4b606649fd
Florent Uzio <fuzio@ripple.com>
2024-11-11 11:30:04 +0400

    NFTokenMint Flatten and tests

--- xrpl/transaction/nftoken_mint.go
+++ xrpl/transaction/nftoken_mint.go
@@ -4,19 +4,111 @@ import (
 	"github.com/Peersyst/xrpl-go/xrpl/transaction/types"
 )
 
+// The NFTokenMint transaction creates a non-fungible token and adds it to the relevant NFTokenPage object of the NFTokenMinter as an NFToken object.
+// This transaction is the only opportunity the NFTokenMinter has to specify any token fields that are defined as immutable (for example, the TokenFlags).
+//
+// Example:
+//
+// ```json
+//
+//	{
+//		"TransactionType": "NFTokenMint",
+//		"Account": "rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B",
+//		"TransferFee": 314,
+//		"NFTokenTaxon": 0,
+//		"Flags": 8,
+//		"Fee": "10",
+//		"URI": "697066733A2F2F62616679626569676479727A74357366703775646D37687537367568377932366E6634646675796C71616266336F636C67747179353566627A6469",
+//		"Memos": [
+//			  {
+//				  "Memo": {
+//					  "MemoType":
+//						"687474703A2F2F6578616D706C652E636F6D2F6D656D6F2F67656E65726963",
+//					  "MemoData": "72656E74"
+//				  }
+//			  }
+//		  ]
+//	  }
+//
+// ```
 type NFTokenMint struct {
 	BaseTx
-	NFTokenTaxon uint
-	Issuer       types.Address    `json:",omitempty"`
-	TransferFee  uint16           `json:",omitempty"`
-	URI          types.NFTokenURI `json:",omitempty"`
+	// An arbitrary taxon, or shared identifier, for a series or collection of related NFTs. To mint a series of NFTs, give them all the same taxon.
+	NFTokenTaxon uint32
+	// (Optional) The issuer of the token, if the sender of the account is issuing it on behalf of another account.
+	// This field must be omitted if the account sending the transaction is the issuer of the NFToken.
+	// If provided, the issuer's AccountRoot object must have the NFTokenMinter field set to the sender of this transaction (this transaction's Account field).
+	Issuer types.Address `json:",omitempty"`
+	// (Optional) The value specifies the fee charged by the issuer for secondary sales of the NFToken, if such sales are allowed.
+	// Valid values for this field are between 0 and 50000 inclusive, allowing transfer rates of between 0.00% and 50.00% in increments of 0.001.
+	// If this field is provided, the transaction MUST have the tfTransferable flag enabled.
+	TransferFee uint16 `json:",omitempty"`
+	// (Optional) Up to 256 bytes of arbitrary data. In JSON, this should be encoded as a string of hexadecimal.
+	// You can use the xrpl.convertStringToHex utility to convert a URI to its hexadecimal equivalent.
+	// This is intended to be a URI that points to the data or metadata associated with the NFT.
+	// The contents could decode to an HTTP or HTTPS URL, an IPFS URI, a magnet link, immediate data encoded as an RFC 2379 "data" URL, or even an issuer-specific encoding.
+	// The URI is NOT checked for validity.
+	URI types.NFTokenURI `json:",omitempty"`
 }
 
+// **********************************
+// OfferCreate Flags
+// **********************************
+
+const (
+	// Allow the issuer (or an entity authorized by the issuer) to destroy the minted NFToken. (The NFToken's owner can always do so.)
+	tfBurnable uint = 1
+	// The minted NFToken can only be bought or sold for XRP. This can be desirable if the token has a transfer fee and the issuer does not want to receive fees in non-XRP currencies.
+	tfOnlyXRP uint = 2
+	// DEPRECATED Automatically create trust lines from the issuer to hold transfer fees received from transferring the minted NFToken. The fixRemoveNFTokenAutoTrustLine amendment makes it invalid to set this flag.
+	tfTrustLine uint = 4
+	// The minted NFToken can be transferred to others. If this flag is not enabled, the token can still be transferred from or to the issuer, but a transfer to the issuer must be made based on a buy offer from the issuer and not a sell offer from the NFT holder.
+	tfTransferable uint = 8
+)
+
+// Allow the issuer (or an entity authorized by the issuer) to destroy the minted NFToken. (The NFToken's owner can always do so.)
+func (n *NFTokenMint) SetBurnableFlag() {
+	n.Flags |= tfBurnable
+}
+
+// The minted NFToken can only be bought or sold for XRP. This can be desirable if the token has a transfer fee and the issuer does not want to receive fees in non-XRP currencies.
+func (n *NFTokenMint) SetOnlyXRPFlag() {
+	n.Flags |= tfOnlyXRP
+}
+
+// DEPRECATED Automatically create trust lines from the issuer to hold transfer fees received from transferring the minted NFToken. The fixRemoveNFTokenAutoTrustLine amendment makes it invalid to set this flag.
+func (n *NFTokenMint) SetTrustlineFlag() {
+	n.Flags |= tfTrustLine
+}
+
+// The minted NFToken can be transferred to others. If this flag is not enabled, the token can still be transferred from or to the issuer, but a transfer to the issuer must be made based on a buy offer from the issuer and not a sell offer from the NFT holder.
+func (n *NFTokenMint) SetTransferableFlag() {
+	n.Flags |= tfTransferable
+}
+
+// TxType returns the type of the transaction (NFTokenMint).
 func (*NFTokenMint) TxType() TxType {
 	return NFTokenMintTx
 }
 
-// TODO: Implement flatten
-func (s *NFTokenMint) Flatten() FlatTransaction {
-	return nil
+// Flatten returns a map of the NFTokenMint transaction fields.
+func (n *NFTokenMint) Flatten() FlatTransaction {
+	flattened := n.BaseTx.Flatten()
+
+	flattened["TransactionType"] = "NFTokenMint"
+	flattened["NFTokenTaxon"] = n.NFTokenTaxon
+
+	if n.Issuer != "" {
+		flattened["Issuer"] = n.Issuer
+	}
+
+	if n.TransferFee != 0 {
+		flattened["TransferFee"] = n.TransferFee
+	}
+
+	if n.URI != "" {
+		flattened["URI"] = n.URI
+	}
+
+	return flattened
 }
--- xrpl/transaction/nftoken_mint_test.go
+++ xrpl/transaction/nftoken_mint_test.go
@@ -1,1 +1,139 @@
 package transaction
+
+import (
+	"testing"
+
+	"github.com/Peersyst/xrpl-go/xrpl/testutil"
+	"github.com/Peersyst/xrpl-go/xrpl/transaction/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNFTokenMint_TxType(t *testing.T) {
+	tx := &NFTokenMint{}
+	assert.Equal(t, NFTokenMintTx, tx.TxType())
+}
+
+func TestNFTokenMint_Flags(t *testing.T) {
+	tests := []struct {
+		name     string
+		setter   func(*NFTokenMint)
+		expected uint
+	}{
+		{
+			name: "pass - SetBurnableFlag",
+			setter: func(n *NFTokenMint) {
+				n.SetBurnableFlag()
+			},
+			expected: tfBurnable,
+		},
+		{
+			name: "pass - SetOnlyXRPFlag",
+			setter: func(n *NFTokenMint) {
+				n.SetOnlyXRPFlag()
+			},
+			expected: tfOnlyXRP,
+		},
+		{
+			name: "pass - SetTrustlineFlag",
+			setter: func(n *NFTokenMint) {
+				n.SetTrustlineFlag()
+			},
+			expected: tfTrustLine,
+		},
+		{
+			name: "pass - SetTransferableFlag",
+			setter: func(n *NFTokenMint) {
+				n.SetTransferableFlag()
+			},
+			expected: tfTransferable,
+		},
+		{
+			name: "pass - SetBurnableFlag and SetTransferableFlag",
+			setter: func(n *NFTokenMint) {
+				n.SetBurnableFlag()
+				n.SetTransferableFlag()
+			},
+			expected: tfBurnable | tfTransferable,
+		},
+		{
+			name: "pass - SetBurnableFlag and SetTransferableFlag and SetOnlyXRPFlag",
+			setter: func(n *NFTokenMint) {
+				n.SetBurnableFlag()
+				n.SetTransferableFlag()
+				n.SetOnlyXRPFlag()
+			},
+			expected: tfBurnable | tfTransferable | tfOnlyXRP,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &NFTokenMint{}
+			tt.setter(p)
+			if p.Flags != tt.expected {
+				t.Errorf("Expected Flags to be %d, got %d", tt.expected, p.Flags)
+			}
+		})
+	}
+}
+
+func TestNFTokenMint_Flatten(t *testing.T) {
+	tests := []struct {
+		name     string
+		nft      *NFTokenMint
+		expected string
+	}{
+		{
+			name: "Flatten with all fields",
+			nft: &NFTokenMint{
+				BaseTx: BaseTx{
+					Account: "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+					Fee:     types.XRPCurrencyAmount(10),
+				},
+				NFTokenTaxon: 12345,
+				Issuer:       "rsA2LpzuawewSBQXkiju3YQTMzW13pAAdW",
+				TransferFee:  314,
+				URI:          "697066733A2F2F62616679626569676479727A74357366703775646D37687537367568377932366E6634646675796C71616266336F636C67747179353566627A6469",
+			},
+			expected: `{
+				"Account":         "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				"TransactionType": "NFTokenMint",
+				"Fee":             "10",
+				"NFTokenTaxon":    12345,
+				"Issuer":          "rsA2LpzuawewSBQXkiju3YQTMzW13pAAdW",
+				"TransferFee":     314,
+				"URI":             "697066733A2F2F62616679626569676479727A74357366703775646D37687537367568377932366E6634646675796C71616266336F636C67747179353566627A6469"
+			}`,
+		},
+		{
+			name: "Flatten with minimal fields",
+			nft: &NFTokenMint{
+				BaseTx: BaseTx{
+					Account:         "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+					TransactionType: NFTokenMintTx,
+					Fee:             types.XRPCurrencyAmount(10),
+				},
+				NFTokenTaxon: 12345,
+			},
+			expected: `{
+				"Account":         "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				"TransactionType": "NFTokenMint",
+				"Fee":             "10",
+				"NFTokenTaxon":    12345
+			}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for _, tt := range tests {
+				t.Run(tt.name, func(t *testing.T) {
+					err := testutil.CompareFlattenAndExpected(tt.nft.Flatten(), []byte(tt.expected))
+					if err != nil {
+						t.Error(err)
+					}
+				})
+			}
+		})
+	}
+}
