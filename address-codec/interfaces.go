8b3ad37b2549c5bffa8046f56b2907179e945931
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-11-12 11:58:20 +0100

    refactor(address-codec): package errors and its usages

--- address-codec/interfaces.go
+++
@@ -1,7 +0,0 @@
-package addresscodec
-
-type CryptoImplementation interface {
-	DeriveKeypair(decodedSeed []byte, validator bool) (string, string, error)
-	Sign(msg, privKey string) (string, error)
-	Validate(msg, pubkey, sig string) bool
-}
