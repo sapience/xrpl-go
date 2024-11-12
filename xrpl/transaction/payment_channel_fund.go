fba6f4e00ea1aac11a1754f5d0af8f996796c391
Florent Uzio <fuzio@ripple.com>
2024-11-12 16:31:21 +0400

    Channel Fund Flatten and tests

--- xrpl/transaction/payment_channel_fund.go
+++ xrpl/transaction/payment_channel_fund.go
@@ -4,18 +4,48 @@ import (
 	"github.com/Peersyst/xrpl-go/xrpl/transaction/types"
 )
 
+// Add additional XRP to an open payment channel, and optionally update the expiration time of the channel. Only the source address of the channel can use this transaction.
+//
+// Example:
+//
+// ```json
+//
+// {
+// "Account": "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+// "TransactionType": "PaymentChannelFund",
+// "Channel": "C1AE6DDDEEC05CF2978C0BAD6FE302948E9533691DC749DCDD3B9E5992CA6198",
+// "Amount": "200000",
+// "Expiration": 543171558
+// }
+// ```
 type PaymentChannelFund struct {
 	BaseTx
-	Channel    types.Hash256
-	Amount     types.XRPCurrencyAmount
+	// The unique ID of the channel to fund, as a 64-character hexadecimal string.
+	Channel types.Hash256
+	// Amount of XRP, in drops to add to the channel. Must be a positive amount of XRP.
+	Amount types.XRPCurrencyAmount
+	// (Optional) New Expiration time to set for the channel, in seconds since the Ripple Epoch.
+	// This must be later than either the current time plus the SettleDelay of the channel, or the existing Expiration of the channel.
+	// After the Expiration time, any transaction that would access the channel closes the channel without taking its normal action.
+	//Any unspent XRP is returned to the source address when the channel closes. (Expiration is separate from the channel's immutable CancelAfter time.) For more information, see the PayChannel ledger object type.
 	Expiration uint32 `json:",omitempty"`
 }
 
+// TxType returns the type of the transaction (PaymentChannelFund).
 func (*PaymentChannelFund) TxType() TxType {
 	return PaymentChannelFundTx
 }
 
-// TODO: Implement flatten
+// Flatten returns a map of the PaymentChannelFund transaction fields.
 func (s *PaymentChannelFund) Flatten() FlatTransaction {
-	return nil
+	flattened := s.BaseTx.Flatten()
+
+	flattened["Channel"] = s.Channel.String()
+	flattened["Amount"] = s.Amount.String()
+
+	if s.Expiration != 0 {
+		flattened["Expiration"] = s.Expiration
+	}
+
+	return flattened
 }
--- xrpl/transaction/payment_channel_fund_test.go
+++ xrpl/transaction/payment_channel_fund_test.go
@@ -1,1 +1,68 @@
 package transaction
+
+import (
+	"testing"
+
+	"github.com/Peersyst/xrpl-go/xrpl/testutil"
+	"github.com/Peersyst/xrpl-go/xrpl/transaction/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPaymentChannelFund_TxType(t *testing.T) {
+	tx := &PaymentChannelFund{}
+	assert.Equal(t, PaymentChannelFundTx, tx.TxType())
+}
+
+func TestPaymentChannelFund_Flatten(t *testing.T) {
+	tests := []struct {
+		name     string
+		tx       *PaymentChannelFund
+		expected string
+	}{
+		{
+			name: "pass - Without Expiration",
+			tx: &PaymentChannelFund{
+				BaseTx: BaseTx{
+					Account:         "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+					TransactionType: PaymentChannelFundTx,
+				},
+				Channel: "ABC123",
+				Amount:  types.XRPCurrencyAmount(200000),
+			},
+			expected: `{
+				"Account": "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				"TransactionType": "PaymentChannelFund",
+				"Channel": "ABC123",
+				"Amount":  "200000"
+			}`,
+		},
+		{
+			name: "With Expiration",
+			tx: &PaymentChannelFund{
+				BaseTx: BaseTx{
+					Account:         "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+					TransactionType: PaymentChannelFundTx,
+				},
+				Channel:    "DEF456",
+				Amount:     types.XRPCurrencyAmount(300000),
+				Expiration: 543171558,
+			},
+			expected: `{
+				"Account": "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+				"TransactionType": "PaymentChannelFund",
+				"Channel": "DEF456",
+				"Amount": "300000",
+				"Expiration": 543171558
+			}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := testutil.CompareFlattenAndExpected(tt.tx.Flatten(), []byte(tt.expected))
+			if err != nil {
+				t.Error(err)
+			}
+		})
+	}
+}
