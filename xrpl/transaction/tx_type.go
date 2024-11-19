5be52dd983109c55ad033993e819939ac99580ab
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-11-19 19:21:41 +0100

    feat(xrpl): `XChainClaim` transaction

--- xrpl/transaction/tx_type.go
+++ xrpl/transaction/tx_type.go
@@ -36,6 +36,8 @@ const (
 	TicketCreateTx         TxType = "TicketCreate"
 	HashedTx               TxType = "HASH"   // TX stored as a string, rather than complete tx obj
 	BinaryTx               TxType = "BINARY" // TX stored as a string, json tagged as 'tx_blob'
+	XChainClaimTx          TxType = "XChainClaim"
+	XChainCommitTx         TxType = "XChainCommit"
 )
 
 func (t TxType) String() string {
--- xrpl/transaction/types/xchain_bridge.go
+++ xrpl/transaction/types/xchain_bridge.go
@@ -1,5 +1,18 @@
 package types
 
+import (
+	"errors"
+
+	addresscodec "github.com/Peersyst/xrpl-go/address-codec"
+)
+
+var (
+	ErrInvalidIssuingChainDoorAddress  = errors.New("xchainBridge: invalid issuing chain door address")
+	ErrInvalidIssuingChainIssueAddress = errors.New("xchainBridge: invalid issuing chain issue address")
+	ErrInvalidLockingChainDoorAddress  = errors.New("xchainBridge: invalid locking chain door address")
+	ErrInvalidLockingChainIssueAddress = errors.New("xchainBridge: invalid locking chain issue address")
+)
+
 type XChainBridge struct {
 	// The door account on the issuing chain. For an XRP-XRP bridge, this must be the
 	// genesis account (the account that is created when the network is first started, which contains all of the XRP).
@@ -11,4 +24,34 @@ type XChainBridge struct {
 	LockingChainDoor Address
 	// The asset that is locked and unlocked on the locking chain.
 	LockingChainIssue Address
+}
+
+type FlatXChainBridge map[string]string
+
+func (x *XChainBridge) Flatten() FlatXChainBridge {
+	flat := make(FlatXChainBridge)
+
+	flat["IssuingChainDoor"] = x.IssuingChainDoor.String()
+	flat["IssuingChainIssue"] = x.IssuingChainIssue.String()
+	flat["LockingChainDoor"] = x.LockingChainDoor.String()
+	flat["LockingChainIssue"] = x.LockingChainIssue.String()
+
+	return flat
+}
+
+func (x *XChainBridge) Validate() (bool, error) {
+	if !addresscodec.IsValidClassicAddress(x.IssuingChainDoor.String()) {
+		return false, ErrInvalidIssuingChainDoorAddress
+	}
+	if !addresscodec.IsValidClassicAddress(x.IssuingChainIssue.String()) {
+		return false, ErrInvalidIssuingChainIssueAddress
+	}
+	if !addresscodec.IsValidClassicAddress(x.LockingChainDoor.String()) {
+		return false, ErrInvalidLockingChainDoorAddress
+	}
+	if !addresscodec.IsValidClassicAddress(x.LockingChainIssue.String()) {
+		return false, ErrInvalidLockingChainIssueAddress
+	}
+
+	return true, nil
 }
