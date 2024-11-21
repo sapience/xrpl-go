a75edb888a2778a5c6da312da1b4638ab33bb398
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-11-21 15:54:43 +0100

    fix(address-codec): `ripemd160` package

--- address-codec/sha256.go
+++ address-codec/sha256.go
@@ -3,8 +3,7 @@ package addresscodec
 import (
 	"crypto/sha256"
 
-	//nolint:staticcheck // G406: Use of deprecated weak cryptographic primitive (gosec)
-	"golang.org/x/crypto/ripemd160"
+	"github.com/decred/dcrd/crypto/ripemd160"
 )
 
 // Returns byte slice of a double hashed given byte slice.
@@ -13,8 +12,6 @@ func Sha256RipeMD160(b []byte) []byte {
 	sha256 := sha256.New()
 	sha256.Write(b)
 
-	// TODO: Check if this is still needed
-	//nolint:gosec // G406: Use of deprecated weak cryptographic primitive (gosec)
 	ripemd160 := ripemd160.New()
 	ripemd160.Write(sha256.Sum(nil))
 
