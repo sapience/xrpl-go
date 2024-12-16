082c2b351b19f355c8331ab74645d907c54e72ad
GuillemGarciaDev <guillem.garcia.gil@protonmail.com>
2024-12-16 18:59:11 +0100

    feat(xrpl): add queries reference

--- xrpl/websocket/queries.go
+++ xrpl/websocket/queries.go
@@ -81,6 +81,9 @@ func (c *Client) GetAccountLines(req *account.LinesRequest) (*account.LinesRespo
 	return &acr, nil
 }
 
+// GetAccountNFTs retrieves a list of NFTs owned by an account on the XRP Ledger.
+// It takes an AccountNFTsRequest as input and returns an AccountNFTsResponse,
+// along with any error encountered.
 func (c *Client) GetAccountNFTs(req *account.NFTsRequest) (*account.NFTsResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -94,6 +97,9 @@ func (c *Client) GetAccountNFTs(req *account.NFTsRequest) (*account.NFTsResponse
 	return &acr, nil
 }
 
+// GetAccountCurrencies retrieves a list of currencies that an account can send or receive.
+// It takes an AccountCurrenciesRequest as input and returns an AccountCurrenciesResponse,
+// along with any error encountered.
 func (c *Client) GetAccountCurrencies(req *account.CurrenciesRequest) (*account.CurrenciesResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -107,6 +113,10 @@ func (c *Client) GetAccountCurrencies(req *account.CurrenciesRequest) (*account.
 	return &acr, nil
 }
 
+// GetAccountOffers retrieves a list of offers made by an account that are currently active
+// in the XRP Ledger's decentralized exchange.
+// It takes an AccountOffersRequest as input and returns an AccountOffersResponse,
+// along with any error encountered.
 func (c *Client) GetAccountOffers(req *account.OffersRequest) (*account.OffersResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -120,6 +130,9 @@ func (c *Client) GetAccountOffers(req *account.OffersRequest) (*account.OffersRe
 	return &acr, nil
 }
 
+// GetAccountTransactions retrieves a list of transactions that involved a specific account.
+// It takes an AccountTransactionsRequest as input and returns an AccountTransactionsResponse,
+// along with any error encountered.
 func (c *Client) GetAccountTransactions(req *account.TransactionsRequest) (*account.TransactionsResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -135,6 +148,9 @@ func (c *Client) GetAccountTransactions(req *account.TransactionsRequest) (*acco
 
 // Channel queries
 
+// GetChannelVerify verifies the signature of a payment channel claim.
+// It takes a ChannelVerifyRequest as input and returns a ChannelVerifyResponse,
+// along with any error encountered.
 func (c *Client) GetChannelVerify(req *channel.VerifyRequest) (*channel.VerifyResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -150,7 +166,8 @@ func (c *Client) GetChannelVerify(req *channel.VerifyRequest) (*channel.VerifyRe
 
 // Ledger queries
 
-// Returns the index of the most recently validated ledger.
+// GetLedgerIndex returns the index of the most recently validated ledger.
+// It returns the ledger index as a LedgerIndex type and any error encountered.
 func (c *Client) GetLedgerIndex() (common.LedgerIndex, error) {
 	res, err := c.Request(&ledger.Request{
 		LedgerIndex: common.LedgerTitle("validated"),
@@ -167,6 +184,8 @@ func (c *Client) GetLedgerIndex() (common.LedgerIndex, error) {
 	return lr.LedgerIndex, err
 }
 
+// GetClosedLedger retrieves information about the last closed ledger.
+// It returns a ClosedResponse containing the ledger information and any error encountered.
 func (c *Client) GetClosedLedger() (*ledger.ClosedResponse, error) {
 	res, err := c.Request(&ledger.ClosedRequest{})
 	if err != nil {
@@ -180,6 +199,8 @@ func (c *Client) GetClosedLedger() (*ledger.ClosedResponse, error) {
 	return &lr, nil
 }
 
+// GetCurrentLedger retrieves information about the current working ledger.
+// It returns a CurrentResponse containing the ledger information and any error encountered.
 func (c *Client) GetCurrentLedger() (*ledger.CurrentResponse, error) {
 	res, err := c.Request(&ledger.CurrentRequest{})
 	if err != nil {
@@ -193,6 +214,9 @@ func (c *Client) GetCurrentLedger() (*ledger.CurrentResponse, error) {
 	return &lr, nil
 }
 
+// GetLedgerData retrieves contents of a ledger.
+// It takes a DataRequest as input and returns a DataResponse containing the ledger data,
+// along with any error encountered.
 func (c *Client) GetLedgerData(req *ledger.DataRequest) (*ledger.DataResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -206,6 +230,9 @@ func (c *Client) GetLedgerData(req *ledger.DataRequest) (*ledger.DataResponse, e
 	return &lr, nil
 }
 
+// GetLedger retrieves information about a specific ledger version.
+// It takes a Request as input and returns a Response containing the ledger information,
+// along with any error encountered.
 func (c *Client) GetLedger(req *ledger.Request) (*ledger.Response, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -221,6 +248,9 @@ func (c *Client) GetLedger(req *ledger.Request) (*ledger.Response, error) {
 
 // NFT queries
 
+// GetNFTBuyOffers retrieves all buy offers for a specific NFT.
+// It takes an NFTokenBuyOffersRequest as input and returns an NFTokenBuyOffersResponse,
+// along with any error encountered.
 func (c *Client) GetNFTBuyOffers(req *nft.NFTokenBuyOffersRequest) (*nft.NFTokenBuyOffersResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -234,6 +264,9 @@ func (c *Client) GetNFTBuyOffers(req *nft.NFTokenBuyOffersRequest) (*nft.NFToken
 	return &lr, nil
 }
 
+// GetNFTSellOffers retrieves all sell offers for a specific NFT.
+// It takes an NFTokenSellOffersRequest as input and returns an NFTokenSellOffersResponse,
+// along with any error encountered.
 func (c *Client) GetNFTSellOffers(req *nft.NFTokenSellOffersRequest) (*nft.NFTokenSellOffersResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -249,6 +282,9 @@ func (c *Client) GetNFTSellOffers(req *nft.NFTokenSellOffersRequest) (*nft.NFTok
 
 // Path queries
 
+// GetBookOffers retrieves a list of offers between two currencies.
+// It takes a BookOffersRequest as input and returns a BookOffersResponse,
+// along with any error encountered.
 func (c *Client) GetBookOffers(req *path.BookOffersRequest) (*path.BookOffersResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -262,6 +298,9 @@ func (c *Client) GetBookOffers(req *path.BookOffersRequest) (*path.BookOffersRes
 	return &lr, nil
 }
 
+// GetDepositAuthorized checks whether one account is authorized to send payments directly to another.
+// It takes a DepositAuthorizedRequest as input and returns a DepositAuthorizedResponse,
+// along with any error encountered.
 func (c *Client) GetDepositAuthorized(req *path.DepositAuthorizedRequest) (*path.DepositAuthorizedResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -275,6 +314,9 @@ func (c *Client) GetDepositAuthorized(req *path.DepositAuthorizedRequest) (*path
 	return &lr, nil
 }
 
+// FindPathCreate creates a path finding request that will be monitored until it expires or is closed.
+// It takes a FindCreateRequest as input and returns a FindResponse,
+// along with any error encountered.
 func (c *Client) FindPathCreate(req *path.FindCreateRequest) (*path.FindResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -288,6 +330,9 @@ func (c *Client) FindPathCreate(req *path.FindCreateRequest) (*path.FindResponse
 	return &lr, nil
 }
 
+// FindPathClose closes an existing path finding request.
+// It takes a FindCloseRequest as input and returns a FindResponse,
+// along with any error encountered.
 func (c *Client) FindPathClose(req *path.FindCloseRequest) (*path.FindResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -301,6 +346,9 @@ func (c *Client) FindPathClose(req *path.FindCloseRequest) (*path.FindResponse, 
 	return &lr, nil
 }
 
+// FindPathStatus checks the status of an existing path finding request.
+// It takes a FindStatusRequest as input and returns a FindResponse,
+// along with any error encountered.
 func (c *Client) FindPathStatus(req *path.FindStatusRequest) (*path.FindResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -314,6 +362,9 @@ func (c *Client) FindPathStatus(req *path.FindStatusRequest) (*path.FindResponse
 	return &lr, nil
 }
 
+// GetRipplePathFind finds paths for a payment between two accounts.
+// It takes a RipplePathFindRequest as input and returns a RipplePathFindResponse,
+// along with any error encountered.
 func (c *Client) GetRipplePathFind(req *path.RipplePathFindRequest) (*path.RipplePathFindResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -345,6 +396,9 @@ func (c *Client) GetServerInfo(req *server.InfoRequest) (*server.InfoResponse, e
 	return &sir, err
 }
 
+// GetAllFeatures retrieves information about all features supported by the server.
+// It takes a FeatureAllRequest as input and returns a FeatureAllResponse,
+// along with any error encountered.
 func (c *Client) GetAllFeatures(req *server.FeatureAllRequest) (*server.FeatureAllResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -358,6 +412,9 @@ func (c *Client) GetAllFeatures(req *server.FeatureAllRequest) (*server.FeatureA
 	return &lr, nil
 }
 
+// GetFeature retrieves information about a specific feature supported by the server.
+// It takes a FeatureOneRequest as input and returns a FeatureResponse,
+// along with any error encountered.
 func (c *Client) GetFeature(req *server.FeatureOneRequest) (*server.FeatureResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -371,6 +428,9 @@ func (c *Client) GetFeature(req *server.FeatureOneRequest) (*server.FeatureRespo
 	return &lr, nil
 }
 
+// GetFee retrieves the current transaction fee settings from the server.
+// It takes a FeeRequest as input and returns a FeeResponse,
+// along with any error encountered.
 func (c *Client) GetFee(req *server.FeeRequest) (*server.FeeResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -384,6 +444,9 @@ func (c *Client) GetFee(req *server.FeeRequest) (*server.FeeResponse, error) {
 	return &lr, nil
 }
 
+// GetManifest retrieves public information about a known validator.
+// It takes a ManifestRequest as input and returns a ManifestResponse,
+// along with any error encountered.
 func (c *Client) GetManifest(req *server.ManifestRequest) (*server.ManifestResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -397,6 +460,9 @@ func (c *Client) GetManifest(req *server.ManifestRequest) (*server.ManifestRespo
 	return &lr, nil
 }
 
+// GetServerState retrieves information about the current state of the server.
+// It takes a StateRequest as input and returns a StateResponse,
+// along with any error encountered.
 func (c *Client) GetServerState(req *server.StateRequest) (*server.StateResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -412,6 +478,9 @@ func (c *Client) GetServerState(req *server.StateRequest) (*server.StateResponse
 
 // Utility queries
 
+// Ping tests the connection to the server.
+// It takes a PingRequest as input and returns a PingResponse,
+// along with any error encountered.
 func (c *Client) Ping(req *utility.PingRequest) (*utility.PingResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
@@ -425,6 +494,9 @@ func (c *Client) Ping(req *utility.PingRequest) (*utility.PingResponse, error) {
 	return &lr, nil
 }
 
+// GetRandom provides a random number from the server.
+// It takes a RandomRequest as input and returns a RandomResponse,
+// along with any error encountered.
 func (c *Client) GetRandom(req *utility.RandomRequest) (*utility.RandomResponse, error) {
 	res, err := c.Request(req)
 	if err != nil {
