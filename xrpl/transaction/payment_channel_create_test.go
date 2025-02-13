996ab280a13c6e9c563cfef4f28b0377ef8a6a15
Florent Uzio <fuzio@ripple.com>
2025-02-13 15:06:31 +0400

    paymentChannelCreate unmarshal test

--- xrpl/transaction/payment_channel_create_test.go
+++ xrpl/transaction/payment_channel_create_test.go
@@ -1,6 +1,8 @@
 package transaction
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/Peersyst/xrpl-go/xrpl/testutil"
@@ -203,6 +205,110 @@ func TestPaymentChannelCreate_Validate(t *testing.T) {
 			} else {
 				require.NoError(t, err)
 				require.Equal(t, tt.wantValid, valid)
+			}
+		})
+	}
+}
+
+func TestPaymentChannelCreate_Unmarshal(t *testing.T) {
+	tests := []struct {
+		name                 string
+		jsonData             string
+		expectedTag          *uint32
+		expectUnmarshalError bool
+	}{
+		{
+			name: "pass - full PaymentChannelCreate with DestinationTag",
+			jsonData: `{
+				"TransactionType": "PaymentChannelCreate",
+				"Account": "rEXAMPLE123456789ABCDEFGHJKLMNPQRSTUVWXYZ",
+				"Destination": "rDEST123456789ABCDEFGHJKLMNPQRSTUVWXYZ",
+				"Amount": "1000000",
+				"Fee": "10",
+				"Sequence": 1,
+				"Flags": 2147483648,
+				"CancelAfter": 695123456,
+				"FinishAfter": 695000000,
+				"Condition": "A0258020C4F71E9B01F5A78023E932ABF6B2C1F020986E6C9E55678FFBAE67A2F5B474680103080000000000000000000000000000000000000000000000000000000000000000",
+				"DestinationTag": 12345,
+				"SourceTag": 54321,
+				"OwnerNode": "0000000000000000",
+				"PreviousTxnID": "C4F71E9B01F5A78023E932ABF6B2C1F020986E6C9E55678FFBAE67A2F5B47468",
+				"LastLedgerSequence": 12345678,
+				"NetworkID": 1024,
+				"Memos": [
+					{
+					"Memo": {
+						"MemoType": "657363726F77",
+						"MemoData": "457363726F77206372656174656420666F72207061796D656E74"
+					}
+					}
+				],
+				"Signers": [
+					{
+					"Signer": {
+						"Account": "rSIGNER123456789ABCDEFGHJKLMNPQRSTUVWXYZ",
+						"SigningPubKey": "ED5F93AB1234567890ABCDEF1234567890ABCDEF1234567890ABCDEF12345678",
+						"TxnSignature": "3045022100D7F67A81F343...B87D"
+					}
+					}
+				],
+				"SigningPubKey": "ED5F93AB1234567890ABCDEF1234567890ABCDEF1234567890ABCDEF12345678",
+				"TxnSignature": "3045022100D7F67A81F343...B87D"
+			}`,
+			expectedTag:          func() *uint32 { v := uint32(12345); return &v }(),
+			expectUnmarshalError: false,
+		},
+		{
+			name: "pass - partial PaymentChannelCreate with DestinationTag set to 0",
+			jsonData: `{
+				"TransactionType": "PaymentChannelCreate",
+				"Account": "rEXAMPLE123456789ABCDEFGHJKLMNPQRSTUVWXYZ",
+				"Destination": "rDEST123456789ABCDEFGHJKLMNPQRSTUVWXYZ",
+				"Amount": "1000000",
+				"Fee": "10",
+				"Sequence": 1,
+				"Flags": 2147483648,
+				"CancelAfter": 695123456,
+				"FinishAfter": 695000000,
+				"Condition": "A0258020C4F71E9B01F5A78023E932ABF6B2C1F020986E6C9E55678FFBAE67A2F5B474680103080000000000000000000000000000000000000000000000000000000000000000",
+				"DestinationTag": 0
+			}`,
+			expectedTag:          func() *uint32 { v := uint32(0); return &v }(),
+			expectUnmarshalError: false,
+		},
+		{
+			name: "pass - partial PaymentChannelCreate with DestinationTag undefined",
+			jsonData: `{
+				"TransactionType": "PaymentChannelCreate",
+				"Account": "rEXAMPLE123456789ABCDEFGHJKLMNPQRSTUVWXYZ",
+				"Destination": "rDEST123456789ABCDEFGHJKLMNPQRSTUVWXYZ",
+				"Amount": "1000000",
+				"Fee": "10",
+				"Sequence": 1,
+				"Flags": 2147483648,
+				"CancelAfter": 695123456,
+				"FinishAfter": 695000000,
+				"Condition": "A0258020C4F71E9B01F5A78023E932ABF6B2C1F020986E6C9E55678FFBAE67A2F5B474680103080000000000000000000000000000000000000000000000000000000000000000"			}`,
+			expectedTag:          nil,
+			expectUnmarshalError: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var paymentChannelCreate PaymentChannelCreate
+			err := json.Unmarshal([]byte(tt.jsonData), &paymentChannelCreate)
+			fmt.Println(paymentChannelCreate.TransactionType)
+			if (err != nil) != tt.expectUnmarshalError {
+				t.Errorf("Unmarshal() error = %v, expectUnmarshalError %v", err, tt.expectUnmarshalError)
+				return
+			}
+			if tt.expectedTag == nil {
+				require.Nil(t, paymentChannelCreate.DestinationTag, "Expected DestinationTag to be nil")
+			} else {
+				require.NotNil(t, paymentChannelCreate.DestinationTag, "Expected DestinationTag not to be nil")
+				require.Equal(t, *tt.expectedTag, *paymentChannelCreate.DestinationTag)
 			}
 		})
 	}
