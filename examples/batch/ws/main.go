3e9918fcf727adb82e61998b0229e3cc03b48b20
banasa44 <cfbennassar@gmail.com>
2025-07-11 10:01:16 +0200

    feat(examples): add receiver wallet and multisig batch transaction

--- examples/batch/ws/main.go
+++ examples/batch/ws/main.go
@@ -52,6 +52,11 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	receiverWallet, err := wallet.New(crypto.ED25519())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	fmt.Println("⏳ Funding wallets...")
 	if err := client.FundWallet(&userWallet); err != nil {
@@ -74,8 +79,14 @@ func main() {
 		user2Balance = "0"
 	}
 
+	receiverBalance, err := client.GetXrpBalance(receiverWallet.ClassicAddress)
+	if err != nil {
+		receiverBalance = "0"
+	}
+
 	fmt.Printf("💳 User initial balance: %s XRP\n", userBalance)
 	fmt.Printf("💳 User2 initial balance: %s XRP\n", user2Balance)
+	fmt.Printf("💳 Receiver initial balance: %s XRP\n", receiverBalance)
 	fmt.Println()
 
 	fmt.Printf("Batch transaction test\n")
@@ -87,16 +98,8 @@ func main() {
 			TransactionType: transaction.BatchTx,
 		},
 		RawTransactions: []txnTypes.RawTransaction{
-			{RawTransaction: CreatePaymentTx(&userWallet, &user2Wallet, txnTypes.XRPCurrencyAmount(1000000)).Flatten()},
-			{RawTransaction: CreatePaymentTx(&userWallet, &user2Wallet, txnTypes.XRPCurrencyAmount(5000000)).Flatten()},
-		},
-		BatchSigners: []txnTypes.BatchSigner{
-			{
-				BatchSigner: txnTypes.BatchSignerData{
-					Account:       txnTypes.Address(user2Wallet.ClassicAddress),
-					SigningPubKey: user2Wallet.PublicKey,
-				},
-			},
+			{RawTransaction: CreatePaymentTx(&userWallet, &receiverWallet, txnTypes.XRPCurrencyAmount(5000000)).Flatten()},
+			{RawTransaction: CreatePaymentTx(&userWallet, &receiverWallet, txnTypes.XRPCurrencyAmount(5000000)).Flatten()},
 		},
 	}
 	batchTx.SetAllOrNothingFlag()
@@ -109,7 +112,6 @@ func main() {
 	}
 
 	fmt.Println("⏳ Signing batch transaction...")
-
 	response, err := client.SubmitTxAndWait(flattenedBatchTx, &types.SubmitOptions{
 		Autofill: false,
 		Wallet:   &userWallet,
@@ -129,11 +131,80 @@ func main() {
 	if err != nil {
 		finalUserBalance = "0"
 	}
+	finalReceiverBalance, err := client.GetXrpBalance(receiverWallet.ClassicAddress)
+	if err != nil {
+		finalReceiverBalance = "0"
+	}
+
+	fmt.Printf("💳 User final balance: %s XRP\n", finalUserBalance)
+
+	fmt.Printf("💳 Receiver final balance: %s XRP\n", finalReceiverBalance)
+
+	fmt.Println()
+	fmt.Printf("Multisig Batch transaction test\n")
+
+	// Create test batch transaction
+	multiBatchTx := &transaction.Batch{
+		BaseTx: transaction.BaseTx{
+			Account:         txnTypes.Address(userWallet.ClassicAddress),
+			TransactionType: transaction.BatchTx,
+		},
+		RawTransactions: []txnTypes.RawTransaction{
+			{RawTransaction: CreatePaymentTx(&userWallet, &receiverWallet, txnTypes.XRPCurrencyAmount(5000000)).Flatten()},
+			{RawTransaction: CreatePaymentTx(&user2Wallet, &receiverWallet, txnTypes.XRPCurrencyAmount(5000000)).Flatten()},
+		},
+		BatchSigners: []txnTypes.BatchSigner{
+			{
+				BatchSigner: txnTypes.BatchSignerData{
+					Account:       txnTypes.Address(user2Wallet.ClassicAddress),
+					SigningPubKey: user2Wallet.PublicKey,
+				},
+			},
+		},
+	}
+	multiBatchTx.SetAllOrNothingFlag()
+
+	flattenedMultiBatchTx := multiBatchTx.Flatten()
+	fmt.Println("⏳ Autofilling flattened multi batch transaction...")
+	if err := client.AutofillMultisigned(&flattenedMultiBatchTx, 1); err != nil {
+		fmt.Println("Autofill error:", err)
+		return
+	}
+
+	fmt.Println("⏳ Signing multi batch transaction...")
+	if err := wallet.SignMultiBatch(user2Wallet, &flattenedMultiBatchTx, nil); err != nil {
+		fmt.Println("Signing error:", err)
+		return
+	}
+
+	response, err = client.SubmitTxAndWait(flattenedMultiBatchTx, &types.SubmitOptions{
+		Autofill: false,
+		Wallet:   &userWallet,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("✅ Multisig Batch transaction submitted")
+	fmt.Printf("🌐 Hash: %s\n", response.Hash.String())
+	fmt.Printf("🌐 Validated: %t\n", response.Validated)
+	fmt.Println()
+
+	// Check final balances
 	finalUser2Balance, err := client.GetXrpBalance(user2Wallet.ClassicAddress)
 	if err != nil {
 		finalUser2Balance = "0"
 	}
-
+	finalUserBalance, err = client.GetXrpBalance(userWallet.ClassicAddress)
+	if err != nil {
+		finalUserBalance = "0"
+	}
+	finalReceiverBalance, err = client.GetXrpBalance(receiverWallet.ClassicAddress)
+	if err != nil {
+		finalReceiverBalance = "0"
+	}
 	fmt.Printf("💳 User final balance: %s XRP\n", finalUserBalance)
 	fmt.Printf("💳 User2 final balance: %s XRP\n", finalUser2Balance)
+	fmt.Printf("💳 Receiver final balance: %s XRP\n", finalReceiverBalance)
 }
